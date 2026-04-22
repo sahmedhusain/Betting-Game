488d72ba158ff09af6f1918a1aa8da748967c91a
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2026-04-22 05:24:31 +0300

    style(frontend): reformat tailwind and postcss config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        emerald: {
-          400: '#34d399',
-          500: '#10b981',
-          600: '#059669',
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                emerald: {
+                    400: '#34d399',
+                    500: '#10b981',
+                    600: '#059669',
+                },
+                dark: '#0f172a',
+            },
+            fontFamily: {
+                outfit: ['Outfit', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+            },
         },
-        dark: '#0f172a',
-      },
-      fontFamily: {
-        outfit: ['Outfit', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
